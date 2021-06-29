8a45f8b3a67ecd8b27f723d51e329fd35b657cf6
rishiraj <rishirajsinghchawda@gmail.com>
2021-06-29 15:00:29 +0530

    model refactor & and modify admin

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -2,14 +2,49 @@ from django.contrib import admin
 
 from apps.resume.models import *
 
+
+class EducationAdmin(admin.ModelAdmin):
+    readonly_fields = ('year_of_passing',)
+    search_fields = ['name', ]
+    list_filter = ['percentage_or_grade', ]
+    list_display = ['resume','name', 'percentage_or_grade', 'university', ]
+
+
+class ExperienceAdmin(admin.ModelAdmin):
+    search_fields = ['company_name', 'designation', ]
+    list_filter = ['designation', ]
+    list_display = ['resume','company_name', 'duration', 'designation','role','place' ]
+
+
+class HobbiesAdmin(admin.ModelAdmin):
+    search_fields = ['resume', ]
+    list_display = ['resume', 'name', ]
+
+
+class SkillAdmin(admin.ModelAdmin):
+    search_fields = ['resume', ]
+    list_display = ['resume', 'name', ]
+
+
+class CertificateAdmin(admin.ModelAdmin):
+    search_fields = ['resume', ]
+    list_display = ['resume', 'name', ]
+
+
+
+class AchievementsAdmin(admin.ModelAdmin):
+    search_fields = ['resume', ]
+    list_display = ['resume', 'name', ]
+
+
 admin.site.register(Resume)
 admin.site.register(UserExtraFields)
-admin.site.register(Education)
-admin.site.register(Experience)
-admin.site.register(Hobbies)
-admin.site.register(Skills)
-admin.site.register(Certificate)
-admin.site.register(Achievements)
+admin.site.register(Education, EducationAdmin)
+admin.site.register(Experience, ExperienceAdmin)
+admin.site.register(Hobbies, HobbiesAdmin)
+admin.site.register(Skills, SkillAdmin)
+admin.site.register(Certificate, CertificateAdmin)
+admin.site.register(Achievements, AchievementsAdmin)
 
 
 
