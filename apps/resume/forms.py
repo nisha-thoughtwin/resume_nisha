8135a79fd1a580d652bbbaa7493499327633b50f
Your Name <you@example.com>
2021-06-30 15:30:24 +0530

    update in form.py

--- apps/resume/forms.py
+++ apps/resume/forms.py
@@ -3,53 +3,6 @@ from django import forms
 from .models import Resume, UserExtraFields, Education, Skills, Experience, Hobbies, Certificate, Achievements
 
 
-# class ResumeForm(forms.ModelForm):
-#     class Meta:
-#         model = Resume
-#         fields = '__all__'
-
-# class UserExtraFieldsForm(forms.ModelForm):
-#     class Meta:
-#         model = UserExtraFields
-#         fields = ['date_of_birth','address','phone','photo']
-
-
-# class EducationForm(forms.ModelForm):
-#     class Meta:
-#         model = Education
-#         fields = ['name','year_of_passing','percentage_or_grade','university']
-
-
-# class SkillsForm(forms.ModelForm):
-#     class Meta:
-#         model = Skills
-#         fields = ['name']
-
-
-# class ExperienceForm(forms.ModelForm):
-#     class Meta:
-#         model = Experience
-#         fields = ['company_name','duration','designation','role','place']
-
-
-# class HobbiesForm(forms.ModelForm):
-#     class Meta:
-#         model = Hobbies
-#         fields = ['name']
-
-
-# class CertificateForm(forms.ModelForm):
-#     class Meta:
-#         model = Certificate
-#         fields = ['name']
-
-# class AchievementsForm(forms.ModelForm):
-#     class Meta:
-#         model = Achievements
-#         fields = ['name']
-#
-
-
 class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
